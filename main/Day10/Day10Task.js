69959a7954b14c38b160f00bbac7e49715fb0e60
Faisal Farooq <faisalfarooq.edu@gmail.com>
2022-12-24 07:56:29 +0500

    modified:   main/Day10/Day10.html 	modified:   main/Day10/Day10Task.js 	new file:   main/Day10/Day10Task2.js

--- main/Day10/Day10Task.js
+++ main/Day10/Day10Task.js
@@ -1,16 +1,18 @@
 class Person{
-    nameX='default';
+    name='default';
     gender= 'default';
     age= 0;
     cnic= '0000000000000';
     constructor(name,gender,age,cnic){
-        this.nameX=name;
+        this.name=name;
         this.gender=gender;
         this.age=age;
         this.cnic=cnic;
     }
+
     display(){
-        console.log(nameX+" "+ gender+" "+age+" "+ cnic)
+        console.log("name: "+this.name+" gender: "+ this.gender+" age: "+
+        this.age+" cnic: "+ this.cnic)
     }
 
 }
@@ -37,10 +39,10 @@ class Employee extends Person{
 }
 class Teacher extends Employee{
     classAssigned;
-    Exam;
-    constructor(name, age,gender,cnic, department,salary,designation, exam, classAssigned){
+    course;
+    constructor(name, age,gender,cnic, department,salary,designation, course, classAssigned){
         super(name,age,gender,cnic,department,salary,designation)
-        this.Exam=exam;
+        this.course=course;
         this.classAssigned=classAssigned
     }
 }
@@ -56,22 +58,71 @@ class Staff extends Employee{
 
 const objStudent1= new Student();
 //setting inherited persong attributes
-objStudent1.name="StudentName"
-objStudent1.gender="StudentGender"
-objStudent1.age=20
+objStudent1.name="Faisal"
+objStudent1.gender="male"
+objStudent1.age=25
 objStudent1.cnic="111111111111"
 //now student attributes
 objStudent1.rollNumber=01;
 objStudent1.fee=200;
+
+objStudent1.display();
 console.log(objStudent1.name+" "+ objStudent1.gender+" "+objStudent1.age+" "+ 
             objStudent1.cnic+" "+objStudent1.rollNumber+" "+objStudent1.fee)
-
+//student 2
 const objStudent2= new Student();
+objStudent2.name="Sameer"
+objStudent2.gender="maler"
+objStudent2.age=25
+objStudent2.cnic="222222222222"
+//now student attributes
+objStudent2.rollNumber=01;
+objStudent2.fee=200;
+
+objStudent2.display();
+console.log(objStudent2.name+" "+ objStudent2.gender+" "+objStudent2.age+" "+ 
+            objStudent2.cnic+" "+objStudent2.rollNumber+" "+objStudent2.fee)
+console.log("----------------------TEACHER OBJS---------------------------")
 const objTeacher1= new Teacher();
+objTeacher1.name="Abdullah"
+objTeacher1.gender="Male"
+objTeacher1.age=40
+objTeacher1.cnic="1111111111111111111111111111"
+
+objTeacher1.department="physics"
+objTeacher1.designation="TEACHER"
+objTeacher1.salary=111111
+
+objTeacher1.classAssigned="A"
+objTeacher1.course="Physics"
+
+objTeacher1.display();
+console.log(objTeacher1.name+" "+ objTeacher1.gender+" "+objTeacher1.age+" "+ 
+            objTeacher1.cnic+" "+objTeacher1.department+" "+objTeacher1.designation+
+            " "+objTeacher1.salary+" "+objTeacher1.classAssigned+" "+objTeacher1.course
+            )
+
 const objTeacher2= new Teacher();
+objTeacher2.name="Ahmad"
+objTeacher2.gender="Male"
+objTeacher2.age=42
+objTeacher2.cnic="222222222222222222"
+
+objTeacher2.department="CS"
+objTeacher2.designation="TEACHER"
+objTeacher2.salary=22222222222
+
+objTeacher2.classAssigned="B"
+objTeacher2.course="Algorithms"
+
+objTeacher2.display();
+console.log(objTeacher2.name+" "+ objTeacher2.gender+" "+objTeacher2.age+" "+ 
+            objTeacher2.cnic+" "+objTeacher2.department+" "+objTeacher2.designation+
+            " "+objTeacher2.salary+" "+objTeacher2.classAssigned+" "+objTeacher2.course
+            )
+
 const objStaff1= new Staff();
 const objStaff2= new Staff();
-
 
 
 
---
+++ main/Day10/Day10Task2.js
@@ -0,0 +1,50 @@
+class Employee{
+    name
+    dept
+    constructor(name='', dept='general'){
+        this.name=name;
+        this.dept=dept;
+
+    }
+    display(){
+        console.log("Name "+ this.name + " Department "+this.dept)
+    }
+}
+class Manager extends Employee{
+    reports
+    constructor(name,dept,reports = []){
+        super(name,dept)
+        this.reports=reports;
+    }
+}
+class WorkerBee extends Employee{
+    projects
+    constructor(name,dept,projects=[]){
+        super(name,dept)
+        this.projects=projects;
+    }
+}
+class salesPerson extends WorkerBee{
+    quota
+    dept
+    constructor(projects,quota=100, dept='sales'){
+        super(projects)
+        this.quota=quota;
+        this.dept=dept;
+    }
+}
+class Engineer extends WorkerBee{
+    machine
+    dept
+    constructor(projects,machine='', dept = 'engineering'){
+        super(projects)
+        this.machine=machine;
+        this.dept=dept;
+    }
+}
+
+var objEngineer = new Engineer();
+objEngineer.display();
+
+var objSales = new salesPerson()
+objSales.display();
